be52caa34457ff788a8731e8e771d1ea7fca7a2a
Manoj Bhat <bhatanm@gmail.com>
2020-05-06 19:34:22 +0530

    Styled design details

--- src/components/designDetails.js
+++ src/components/designDetails.js
@@ -3,6 +3,8 @@ import { db } from "../firebaseConfig";
 import queryString from "query-string";
 import Spinner from "react-spinkit";
 import LazyLoad from "react-lazyload";
+import { Link } from "react-router-dom";
+import { textGradient } from "./../utils";
 
 class DesignDetails extends Component {
   state = {
@@ -44,73 +46,91 @@ class DesignDetails extends Component {
       conclusion,
     } = designData;
     return (
-      <div className="design-details">
-        {designData.head ? (
-          <>
-            <h1>{head}</h1>
-            <div className="design-container">
-              <h2>Problem</h2>
-              <p>{problem}</p>
-              <h2>Idea</h2>
-              <p>{idea}</p>
-              <h2>Challenges</h2>
-              <ul>
-                {challenges
-                  ? challenges.map((challenge, index) => (
-                      <li key={index}>{challenge}</li>
-                    ))
-                  : null}
-              </ul>
-              <h2>Approach</h2>
-              <p>{approach}</p>
-              <h2>Length of project</h2>
-              <p>{projectLen}</p>
-              <h2>Research (1st phase)</h2>
-              <p>During the research phase, I wanted to achieve a few goals:</p>
-              <ul>
-                {research0
-                  ? research0.map((research, index) => (
-                      <li key={index}>{research}</li>
-                    ))
-                  : null}
-              </ul>
-              <h2>Research (2nd phase):User Personas</h2>
-              <p>{research1}</p>
-              {personaUrls
-                ? personaUrls.map((persona, index) => (
-                    <LazyLoad key={index}>
-                      <img className="pesona-img" src={persona} alt="persona" />
-                    </LazyLoad>
-                  ))
-                : null}
-              <h2>Inforamtion Architecture</h2>
-              <p>{infoArch}</p>
-              <img
-                src={wireframeUrl}
-                alt="Wire Frame"
-                className="wireframe-img"
-              />
-              <h2>Wireframes: Low-Fidelity</h2>
-              <p>{wireframes0}</p>
-              <h2>Wireframes: High-Fidelity</h2>
-              <p>{wireframes1}</p>
-              {screens
-                ? screens.map((screen, index) => (
-                    <LazyLoad key={index}>
-                      <img src={screen} alt="Screen" className="screen-img" />
-                    </LazyLoad>
-                  ))
-                : null}
-              <h2>Conclusion</h2>
-              <p>{conclusion}</p>
+      <>
+        <div className="design-nav">
+          <h1>Design Details</h1>
+          <div className="resume back-button">
+            <div>
+              <Link to="/home" style={textGradient}>
+                BACK
+              </Link>
             </div>
-          </>
-        ) : (
-          <div className="spinner">
-            <Spinner name="ball-triangle-path" color="#ff6b15" />
           </div>
-        )}
-      </div>
+        </div>
+        <div className="design-details">
+          {designData.head ? (
+            <>
+              <h1>{head}</h1>
+              <div className="design-container">
+                <h2>Problem</h2>
+                <p>{problem}</p>
+                <h2>Idea</h2>
+                <p>{idea}</p>
+                <h2>Challenges</h2>
+                <ul className="bullet-points">
+                  {challenges
+                    ? challenges.map((challenge, index) => (
+                        <li key={index}>{challenge}</li>
+                      ))
+                    : null}
+                </ul>
+                <h2>Approach</h2>
+                <p>{approach}</p>
+                <h2>Length of project</h2>
+                <p>{projectLen}</p>
+                <h2>Research (1st phase)</h2>
+                <p>
+                  During the research phase, I wanted to achieve a few goals:
+                </p>
+                <ul className="bullet-points">
+                  {research0
+                    ? research0.map((research, index) => (
+                        <li key={index}>{research}</li>
+                      ))
+                    : null}
+                </ul>
+                <h2>Research (2nd phase):User Personas</h2>
+                <p>{research1}</p>
+                <div className="persona">
+                  {personaUrls
+                    ? personaUrls.map((persona, index) => (
+                        <LazyLoad key={index}>
+                          <img src={persona} alt="persona" />
+                        </LazyLoad>
+                      ))
+                    : null}
+                </div>
+                <h2>Inforamtion Architecture</h2>
+                <p>{infoArch}</p>
+                <img
+                  src={wireframeUrl}
+                  alt="Wire Frame"
+                  className="wireframe-img"
+                />
+                <h2>Wireframes: Low-Fidelity</h2>
+                <p>{wireframes0}</p>
+                <h2>Wireframes: High-Fidelity</h2>
+                <p>{wireframes1}</p>
+                <div className="screens">
+                  {screens
+                    ? screens.map((screen, index) => (
+                        <LazyLoad key={index}>
+                          <img src={screen} alt="Screen" />
+                        </LazyLoad>
+                      ))
+                    : null}
+                </div>
+                <h2>Conclusion</h2>
+                <p>{conclusion}</p>
+              </div>
+            </>
+          ) : (
+            <div className="spinner des-spinner">
+              <Spinner name="ball-triangle-path" color="#ff6b15" />
+            </div>
+          )}
+        </div>
+      </>
     );
   }
 }
